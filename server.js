d8ea27515e0e2e6b691c95530b3046286c49b85c
Marcio <montimo@gmail.com>
2019-09-03 00:41:44 -0300

    Primeiro projeto API

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const mongoose = require ("mongoose");
+const requireDir = require("require-dir");
+const cors = require ("cors");
+
+//Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando o DB
+mongoose.connect(
+     'mongodb+srv://admin:teste123@cluster0-yfcsr.mongodb.net/test?retryWrites=true&w=majority', 
+     { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require("./src/routes"));
+
+app.listen(3001);
